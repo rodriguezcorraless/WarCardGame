78b69bdd7b6c66b67e5aa4be9be561a25388c9e5
Anthony G. Morganelli <morganellia@wit.edu>
2018-10-30 10:52:13 -0400

    Constructed card class for use with Pile

--- src/edu/wit/cs/comp2000/Card.java
+++ src/edu/wit/cs/comp2000/Card.java
@@ -3,36 +3,58 @@
  */
 package edu.wit.cs.comp2000;
 
-
 /**
- * @author 
+ * @author Anthony & Sancho
  *
  */
 public class Card implements Comparable<Card> {
+	private String name;
+	private int value;
+	private String suit;
 
 	/**
 	 * 
 	 */
-	public Card() {
-		// TODO Auto-generated constructor stub
+	public Card(Rank r, Suit s) {
+		// construct name
+		this.name = r.getDisplayName() + " of " + s.getDisplayName();
+		// construct value
+		this.value = r.getPoints();
+		// construct suit
+		this.suit = s.getDisplayName();
 	}
-
 
 	/*
 	 * @see java.lang.Comparable#compareTo(java.lang.Object)
 	 */
 	@Override
 	public int compareTo(Card o) {
-		// TODO Auto-generated method stub
-		return 0 ;
+		// card is smaller = 0
+		if (o.value > this.value) {
+			return 0;
+		} else if (o.value < this.value) {
+			// card is bigger
+			return 1;
+		} else {
+			// cards are the same
+			return 2;
+		}
 	}
-
+	
+	@Override
+	public String toString() {
+		// call name
+		return this.name;
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// test card
+		//Card test = new Card(Rank.ACE, Suit.CLUBS);
+		// test print
+		//System.out.printf("%s", test.toString());
 
 	}
 
